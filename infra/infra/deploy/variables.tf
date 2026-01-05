f06b6ccfe9b3cc17615780a0a23fd21f41413199
thuandao <thuandh@kaopiz.com>
2026-01-05 23:38:45 +0900

    Add ecs created code!

--- infra/infra/deploy/variables.tf
+++ infra/infra/deploy/variables.tf
@@ -22,6 +22,10 @@ variable "db_password" {
   description = "Password for the Terraform database"
 }
 
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
 locals {
   prefix = "${var.prefix}-${terraform.workspace}"
 }
