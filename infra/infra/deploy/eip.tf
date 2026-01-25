211112e4117697e051a7f20cfdc694e219e6629b
thuandao <thuandh@kaopiz.com>
2026-01-25 23:56:48 +0900

    remove not used code!

--- infra/infra/deploy/eip.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_eip" "bastion" {
-  domain = "vpc"
-}
-
-resource "aws_eip_association" "bastion" {
-  allocation_id = aws_eip.bastion.id
-  instance_id   = aws_autoscaling_group.bastion.instances[0]
-}
--- infra/infra/deploy/outputs.tf
+++ infra/infra/deploy/outputs.tf
@@ -1,7 +1,3 @@
 output "api_endpoint" {
   value = aws_route53_record.app.fqdn
 }
-
-output "bastion_host" {
-  value = aws_eip.bastion.public_dns
-}
