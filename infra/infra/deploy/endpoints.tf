217e104f1dbe12599b7d1d35ecc16f686b218b45
thuandao <thuandh@kaopiz.com>
2026-01-29 23:56:48 +0900

    refactor netword file to module-aws

---
+++ infra/infra/deploy/endpoints.tf
@@ -0,0 +1,79 @@
+#########################################################################
+## Endpoints to allow ECS to access ECR, CloudWatch and Systems Manager #
+#########################################################################
+resource "aws_security_group" "endpoint_access" {
+  description = "Access to endpoints"
+  name        = "${local.prefix}-endpoint-access"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    cidr_blocks = [aws_vpc.main.cidr_block]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+  
+  tags = {
+    Name = "${local.prefix}-endpoint-sg"
+  }
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "~> 5.0"
+
+  vpc_id = module.vpc.vpc_id
+
+  security_group_ids = [
+    aws_security_group.endpoint_access.id
+  ]
+
+  subnet_ids = module.vpc.private_subnets
+
+  endpoints = {
+    ecr_api = {
+      service = "ecr.api"
+      private_dns_enabled = true
+      tags = {
+        Name = "${local.prefix}-ecr-api-endpoint"
+      }
+    }
+
+    ecr_dkr = {
+      service = "ecr.dkr"
+      private_dns_enabled = true
+      tags = {
+        Name = "${local.prefix}-ecr-dkr-endpoint"
+      }
+    }
+
+    logs = {
+      service = "logs"
+      private_dns_enabled = true
+      tags = {
+        Name = "${local.prefix}-logs-endpoint"
+      }
+    }
+
+    ssmmessages = {
+      service = "ssmmessages"
+      private_dns_enabled = true
+      tags = {
+        Name = "${local.prefix}-ssmmessages-endpoint"
+      }
+    }
+
+    s3 = {
+      service = "s3"
+      service_type = "Gateway"
+      route_table_ids = [module.vpc.main.default_route_table_id]
+      tags = {
+        Name = "${local.prefix}-s3-endpoint"
+      }
+    }
+  }
+
+  tags = {
+    Project = local.prefix
+  }
+}
---
+++ infra/infra/deploy/vpc.tf
@@ -0,0 +1,32 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${local.prefix}-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs = [
+    "${data.aws_region.current.name}a",
+    "${data.aws_region.current.name}b"
+  ]
+
+  public_subnets = [
+    "10.1.1.0/24",
+    "10.1.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.1.10.0/24",
+    "10.1.11.0/24"
+  ]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = false   # ECS Fargate + endpoints → không cần NAT
+  single_nat_gateway = false
+
+  tags = {
+    Project = local.prefix
+  }
+}
