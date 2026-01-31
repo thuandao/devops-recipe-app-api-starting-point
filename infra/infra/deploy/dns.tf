c0a778c2fb42242ccb4d6e1303ac977ac0129645
thuandao <thuandh@kaopiz.com>
2026-01-31 17:53:40 +0900

    change to module

--- infra/infra/deploy/dns.tf
+++ infra/infra/deploy/dns.tf
@@ -2,45 +2,35 @@ data "aws_route53_zone" "zone" {
   name = "${var.dns_zone_name}."
 }
 
-resource "aws_route53_record" "app" {
-  zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
-  type    = "CNAME"
-  ttl     = "300"
+module "app_dns" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+  version = "~> 2.0"
 
-  records = [aws_lb.api.dns_name]
+  zone_id = data.aws_route53_zone.zone.zone_id
+
+  records = [
+    {
+      name = lookup(var.subdomain, terraform.workspace)
+      type = "CNAME"
+      ttl  = 300
+      records = [aws_lb.api.dns_name]
+    }
+  ]
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = trimsuffix("${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}", ".")
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
+
+  domain_name = trimsuffix("${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}", ".")
+
+  zone_id = data.aws_route53_zone.zone.zone_id
+
   validation_method = "DNS"
 
-  lifecycle {
-    create_before_destroy = true
+  wait_for_validation = true
+
+  tags = {
+    Name = "${local.prefix}-cert"
   }
-}
-
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.zone.zone_id
-}
-
-
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-
-  depends_on = [aws_route53_record.cert_validation]
 }
