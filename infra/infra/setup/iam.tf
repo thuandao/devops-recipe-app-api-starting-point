05e1aad9cd10f4b151eb949f49160ea06a6da878
thuandao <thuandh@kaopiz.com>
2026-02-07 23:15:33 +0900

    refactor code

--- infra/infra/setup/iam.tf
+++ infra/infra/setup/iam.tf
@@ -51,7 +51,6 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
   policy_arn = aws_iam_policy.tf_backend.arn
 }
 
-
 #########################
 # Policy for ECR access #
 #########################
